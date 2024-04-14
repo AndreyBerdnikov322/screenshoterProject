9150499e87f99c5652d9747b18fc0f919f86029a
AndreyBerdnikov322 <146313776+AndreyBerdnikov322@users.noreply.github.com>
2024-04-14 22:17:58 +0500

    Add files via upload
    скрин экрана по кнопке

---
+++ screenshotproj/screenshotproj/mainform.Designer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ScreenCaptureApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.CaptureButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CaptureButton
+            // 
+            this.CaptureButton.Location = new System.Drawing.Point(12, 12);
+            this.CaptureButton.Name = "CaptureButton";
+            this.CaptureButton.Size = new System.Drawing.Size(200, 50);
+            this.CaptureButton.TabIndex = 0;
+            this.CaptureButton.Text = "Сделать скрин";
+            this.CaptureButton.UseVisualStyleBackColor = true;
+            this.CaptureButton.Click += new System.EventHandler(this.CaptureButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 100);
+            this.Controls.Add(this.CaptureButton);
+            this.Name = "MainForm";
+            this.Text = "Screen Capture App";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button CaptureButton;
+    }
+}
+
+
---
+++ screenshotproj/screenshotproj/mainform.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ScreenCaptureApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptureButton_Click(object sender, EventArgs e)
+        {
+            Bitmap screenshot = CaptureScreen();
+            string fileName = $"screenshot_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+            screenshot.Save(fileName, ImageFormat.Png);
+            MessageBox.Show($"Screenshot saved as {fileName}");
+        }
+
+        private Bitmap CaptureScreen()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(bounds.Width, bounds.Height);
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+            }
+            return screenshot;
+        }
+    }
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
